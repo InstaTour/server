c37d1a1a6ca060a9ffeed57acdd0809d41dbe721
RunaticMoon <mwngh3@gmail.com>
2020-04-26 01:42:15 +0900

    [기타] CORS 추가
    swagger에서 cors문제로 api 호출이 안되어 수정

--- handler.js
+++ handler.js
@@ -6,6 +6,10 @@ const awsSdk = awsXRay.captureAWS(require('aws-sdk'));
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
