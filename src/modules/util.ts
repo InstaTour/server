74d806612e8ecf4e1bdba2b0fcfcee66a46ebc37
RunaticMoon <mwngh3@gmail.com>
2020-04-29 18:49:55 +0900

    [테스트] GET Post 테스트 코드 작성
    하드코딩된 4개의 이미지배열을 반환하는 테스트 코드 작성

---
+++ src/modules/util.ts
@@ -0,0 +1,39 @@
+// dotenv fetch
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { ParameterizedContext } from 'koa';
+
+export function isUndefined(obj: Object) {
+  if (typeof obj === 'undefined' && !obj) {
+    return true;
+  }
+  return false;
+}
+
+export function createResponse(
+  ctx: ParameterizedContext,
+  status: statusCode,
+  body: Object,
+  err: String | null = null
+) {
+  ctx.status = status;
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
+  ctx.body = {
+    message: err,
+    data: body,
+  };
+}
+
+export const enum statusCode {
+  success = 200, // 성공 (서버에서 요청한 값 반환)
+  secondSuccess = 201, // 성공 (서버에 새로운 값 작성)
+  processingSuccess = 204, // 성공 (반환값이 없음)
+  requestError = 400, // 실패 (서버에서 요청을 이해 못함)
+  authenticationFailure = 401, // 권한필요
+  authorizationFailure = 403, // 접근불가
+  failure = 404, // 실패 (서버에서 요청한 값을 찾을 수 없음)
+  serverError = 500, // 서버 내부에서 에러 발생
+  dataBaseError = 600, // DB에서 에러 발생
+}
