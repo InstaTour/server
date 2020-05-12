657109bfbde26b9d7f88dbf890e882a4b80303ad
RunaticMoon <mwngh3@gmail.com>
2020-05-13 02:25:02 +0900

    [기능] 사전인증에 DB User 추가기능
    Neo4J의 User레이블 노드에 정보를 넣어 생성한다.

--- src/authorizer/preSingUp.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Callback } from 'aws-lambda';
-
-exports.handler = (event: any, context: Context, callback: Callback) => {
-  event.response.autoConfirmUser = true;
-  event.response.autoVerifyEmail = true; // this is NOT needed if e-mail is not in attributeList
-  event.response.autoVerifyPhone = true; // this is NOT needed if phone # is not in attributeList
-  context.done(undefined, event);
-};
