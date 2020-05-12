401d05911e5d5b5922cad8bd0ce50b858e86b246
RunaticMoon <mwngh3@gmail.com>
2020-05-13 02:24:09 +0900

    [기능] neo4j transaction 함수 생성
    query배열과 parameter배열을 받아서 transaction을 처리한다.
    만약 중간에 에러가 나면 로그를 찍고 rollback을 한다.

--- src/modules/neo4j.ts
+++ src/modules/neo4j.ts
@@ -2,7 +2,8 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import neo4j from 'neo4j-driver';
+import neo4j, { QueryResult } from 'neo4j-driver';
+import { Parameters } from 'neo4j-driver/types/query-runner';
 
 const driver = neo4j.driver(
   process.env.NEO4J_URL as string,
@@ -12,6 +13,32 @@ const driver = neo4j.driver(
   )
 );
 
-const session = driver.session();
+export const enum Query {
+  create_user = 'CREATE (:User {id: $id, nickname: $nickname, email: $email, profile: $profile})',
+  create_post_instagram = 'CREATE (:Post:Instagram {id: $key, img_url: $img_url, content: $content, likes: $likes, date: $date})',
+  create_post_instatour = 'CREATE (:Post:Instatour {id: $key, img_url: $img_url, content: $content, likes: $likes, date: $date})',
+  update_user = 'MATCH (p:User {id: $id}) CALL apoc.create.setProperties(p, $keys, $values) YIELD node RETURN node',
+  update_post = 'MATCH (p:Post {id: $id}) CALL apoc.create.setProperties(p, $keys, $values) YIELD node RETURN node',
+  post_hashtag_relation = 'MERGE (p:Post {id: $pid}) MERGE (t:HashTag {id: $tid}) MERGE (p)-[:TAGGED]->(t)',
+}
 
-export default session;
+export async function tx(querys: Query[], params: Parameters[]) {
+  const session = driver.session();
+  const txc = session.beginTransaction();
+  try {
+    let result: QueryResult[] = [];
+    for (const i in querys) {
+      result.push(await txc.run(querys[i], params[i]));
+    }
+    await txc.commit();
+    console.log('committed');
+    return result;
+  } catch (error) {
+    console.error(error);
+    await txc.rollback();
+    console.log('rolled back');
+    return null;
+  } finally {
+    await session.close();
+  }
+}
